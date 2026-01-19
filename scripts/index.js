8c80794d37755711c169bd9719ca54bc99c2cf4f
Ivan Cervantes <153766241+Ivancvnts@users.noreply.github.com>
2026-01-18 22:31:48 -0600

    Etapa 1

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+let initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  { name: "Latemar", link: "https://code.s3.yandex.net/web-code/latemar.jpg" },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+
+initialCards.forEach((card) => {
+  console.log(card.name);
+});
