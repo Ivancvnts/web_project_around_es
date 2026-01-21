e23811e9dd44dbc61ec859a042f0ae19607eb03c
Ivan Cervantes <153766241+Ivancvnts@users.noreply.github.com>
2026-01-20 21:27:34 -0600

    feat: Implement edit profile functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,72 @@
-let initialCards = [
-  {
-    name: "Valle de Yosemite",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-  },
-  {
-    name: "Lago Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-  },
-  {
-    name: "Montañas Calvas",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
-  },
-  { name: "Latemar", link: "https://code.s3.yandex.net/web-code/latemar.jpg" },
-  {
-    name: "Parque Nacional de la Vanoise",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg",
-  },
-];
+const profileEditBtn = document.querySelector(".profile__edit-button");
 
-initialCards.forEach((card) => {
-  console.log(card.name);
+const editProfilePopup = document.querySelector("#edit-popup");
+const closeEditPopup = editProfilePopup.querySelector(".popup__close");
+const profileForm = editProfilePopup.querySelector(".popup__form");
+
+// Functions to open and close modals/Popups
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+//Fill profile form with current data
+function fillProfileForm() {
+  //Look for input values elements in DOM
+  const nameInput = editProfilePopup.querySelector(".popup__input_type_name");
+  const descriptionInput = editProfilePopup.querySelector(
+    ".popup__input_type_description",
+  );
+  //Get current profile data from DOM
+  const profileTitleEl = document.querySelector(".profile__title");
+  const profileDescriptionEl = document.querySelector(".profile__description");
+
+  //Set input values to current profile data
+  nameInput.value = profileTitleEl.textContent;
+  descriptionInput.value = profileDescriptionEl.textContent;
+}
+
+//Handle opening edit profile popup
+function handleOpenEditModal(editProfilePopup) {
+  openModal(editProfilePopup);
+  fillProfileForm();
+}
+
+// Handle profile form submission
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+
+  //Look for input values elements in DOM
+  const nameInput = editProfilePopup.querySelector(".popup__input_type_name");
+  const descriptionInput = editProfilePopup.querySelector(
+    ".popup__input_type_description",
+  );
+
+  //Get input values
+  const nameValue = nameInput.value;
+  const descriptionValue = descriptionInput.value;
+
+  //Get profile elements in DOM
+  const profileTitleEl = document.querySelector(".profile__title");
+  const profileDescriptionEl = document.querySelector(".profile__description");
+
+  //Update profile data in DOM with input values
+  profileTitleEl.textContent = nameValue;
+  profileDescriptionEl.textContent = descriptionValue;
+}
+
+profileEditBtn.addEventListener("click", () => {
+  handleOpenEditModal(editProfilePopup);
+});
+
+closeEditPopup.addEventListener("click", () => {
+  closeModal(editProfilePopup);
+});
+
+profileForm.addEventListener("submit", (event) => {
+  handleProfileFormSubmit(event);
+  closeModal(editProfilePopup);
 });
